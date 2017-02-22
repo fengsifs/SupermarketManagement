cc55aaac8ec24608532f55181fc75d6b6cbb5a16
fengsi <feiyoshang@gmail.com>
2017-02-22 12:20:43 +0800

    test d3 json

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -1,4 +1,4 @@
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
+var margin = {top: 20, right: 30, bottom: 30, left: 80},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
